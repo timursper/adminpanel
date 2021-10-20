137643e4d20ac93b0fb4faa2734511109d99301b
timursper <91378191+timursper@users.noreply.github.com>
2021-10-21 09:44:18 +1000

    Add files via upload

--- adminpanel.py
+++ adminpanel.py
@@ -1,8 +1,9 @@
-passwords = ["Keramitec20Cube!"]
+passwords = ["Ceramic20Cube!"]
+names = ["Ceramic cube"]
 name = input("Enter name: ")
 password = input("Enter password: ")
 
-if password == passwords[0] and name == "Ceramic cubic":
+if password == passwords[0] and name == "Ceramic cube":
     print(f"Hi! {name}")
 else:
     print("You did not get into the admin panel!")
